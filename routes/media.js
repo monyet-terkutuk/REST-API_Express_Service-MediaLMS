ace98e27eeb931ec76254b05c03f34c15742f0cc
undeath-cyber <gabrielxjonathan.12@gmail.com>
2023-06-19 11:32:05 +0700

    delete image endpoint

--- routes/media.js
+++ routes/media.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const isBase64 = require("is-base64");
 const base64Img = require("base64-img");
+const fs = require("fs");
 
 // panggil model
 const { Media } = require("../models");
@@ -39,7 +40,7 @@ router.post("/", (req, res) => {
 
     const filename = filepath.split("/").pop();
 
-    const media = await Media.create({ image: `imageS/${filename}` });
+    const media = await Media.create({ image: `images/${filename}` });
 
     return res.status(200).json({
       status: "success",
@@ -47,6 +48,33 @@ router.post("/", (req, res) => {
         id: media.id,
         image: `${req.get("host")}/images/${filename}`,
       },
+    });
+  });
+});
+
+// delete image
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const media = await Media.findByPk(id);
+
+  if (!media) {
+    return res.status(404).json({
+      status: "error",
+      message: "media not found",
+    });
+  }
+
+  fs.unlink(`./public/${media.image}`, async (err) => {
+    if (err) {
+      return res.status(400).json({ status: "error", message: err.message });
+    }
+
+    await media.destroy();
+
+    return res.status(200).json({
+      status: "success",
+      message: "image success deleted",
     });
   });
 });
